d3a9d5841a4ed47f4e07091aa68e3879c46836ee
Chris <cwittern@yahoo.com>
2022-10-21 14:59:18 +0900

    removed PI before XML body

--- app/kr2tls.py
+++ app/kr2tls.py
@@ -22,11 +22,7 @@ lang="zho"
 # need the following vars:
 # user, txtid, title, date, branch, today, body, lang
 # body should contain the preformatted content for the body element
-tei_template="""<?xml version="1.0" encoding="UTF-8"?>
-<?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>
-<?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml"
-	schematypens="http://purl.oclc.org/dsdl/schematron"?>
-<TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="{btxtid}">
+tei_template="""<TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="{btxtid}">
   <teiHeader>
       <fileDesc>
          <titleStmt>
